28418bb3b5ec9c7d35f53baed946b0b3da5dfbc8
Sid <izidrobernadas7@gmail.com>
2025-08-01 09:21:30 +0800

    update vite 3

--- config/server.ts
+++ config/server.ts
@@ -13,6 +13,6 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  allowedHosts: ['usadmin.gamevenues.com'], // ✅ add this
+  allowedHosts: ['admin.gamevenues.com'], // ✅ add this
 });
 
