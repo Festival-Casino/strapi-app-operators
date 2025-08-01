f142f7f1761e0e518eea5b5c95766abef2cd1473
Sid <izidrobernadas7@gmail.com>
2025-08-01 09:31:49 +0800

    fixed error 2

--- config/server.ts
+++ config/server.ts
@@ -9,7 +9,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'admin.gamevenues.com'), // ✅ add this
+  url: env('PUBLIC_URL', 'https://admin.gamevenues.com'), // ✅ add this
   app: {
     keys: env.array('APP_KEYS'),
   },
